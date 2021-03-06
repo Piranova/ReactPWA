b5bd0a153dd2fb51623bd5f6d6e1b699ec83eafe
Brian Curtain <dc.brian.07@gmail.com>
2021-03-06 09:29:23 +0800

    close icon

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -90,7 +90,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
           {links}
         </MobileNavLinks>
         <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
+          {showNavLinks ? <CloseIcon tw="w-6 h-6 text-black" /> : <MenuIcon tw="w-6 h-6" />}
         </NavToggle>
       </MobileNavLinksContainer>
     </Header>
